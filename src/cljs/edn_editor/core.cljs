563c105221b7260b6c3a7de20d58316faa51c821
Paul <paul931224@gmail.com>
2018-05-29 05:01:58 +0200

    Jokes aside, demo setup

--- src/cljs/edn_editor/core.cljs
+++ src/cljs/edn_editor/core.cljs
@@ -1,9 +1,9 @@
 (ns edn-editor.core
-    (:require [reagent.core :as reagent :refer [atom]]
-              [secretary.core :as secretary :include-macros true]
-              [accountant.core :as accountant]
-              ;[clojure.edn/reader :refer [read-string]]
-              [cljs.reader :as reader :refer [read-string read]]))
+  (:require [reagent.core :as reagent :refer [atom]]
+            [secretary.core :as secretary :include-macros true]
+            [accountant.core :as accountant]
+  ;[clojure.edn/reader :refer [read-string]]
+            [cljs.reader :as reader :refer [read-string read]]))
 
 ;; -------------------------
 ;; Views
@@ -25,13 +25,13 @@
 (defn try-to-read [data]
   (if (or (and (= "{" (first data)) (= "}" (last data)))
           (and (= "[" (first data)) (= "]" (last data))))
-   (try
-     (read-string data)
-     (catch :default e
-       data))
-     ;(finally data))
+    (try
+      (read-string data)
+      (catch :default e
+        data))
+    ;(finally data))
 
-   data))
+    data))
 
 
 (defn edit-if-want [[the-key the-content]]
@@ -39,24 +39,24 @@
         the-value (atom the-content)]
     (fn [[the-key the-content]]
       (if @edit?
-         [:div.uk-grid-collapse {:data-uk-grid true}
-          [:button.uk-button-default.uk-button-small.uk-button.uk-text-center.uk-padding-small.uk-padding-remove-vertical
-           {:data-uk-icon "check" :on-click #(do
-                                               (swap! editor-atom assoc-in (vec (conj @show-me-da-way the-key))
-                                                      (try-to-read @the-value))
-                                               (reset! edit? false))}]
-          [:input.uk-input.uk-width-expand  {:on-change #(reset! the-value (-> % .-target .-value))
-                                             :default-value (str the-content)}]]
+        [:div.uk-grid-collapse {:data-uk-grid true}
+         [:button.uk-button-default.uk-button-small.uk-button.uk-text-center.uk-padding-small.uk-padding-remove-vertical
+          {:data-uk-icon "check" :on-click #(do
+                                              (swap! editor-atom assoc-in (vec (conj @show-me-da-way the-key))
+                                                     (try-to-read @the-value))
+                                              (reset! edit? false))}]
+         [:input.uk-input.uk-width-expand {:on-change     #(reset! the-value (-> % .-target .-value))
+                                           :default-value (str the-content)}]]
 
-         [:div.uk-inline.uk-width-1-1
-          {:on-click #(reset! edit? true)}
-          (str the-content)
-          [:span.uk-label.uk-label-danger.uk-position-right (cond
-                                                             (map? (get-in @editor-atom (vec (conj @show-me-da-way the-key))))
-                                                             "map"
-                                                             (vector? (get-in @editor-atom (vec (conj @show-me-da-way the-key))))
-                                                             "vector"
-                                                             :else "string")]]))))
+        [:div.uk-inline.uk-width-1-1
+         {:on-click #(reset! edit? true)}
+         (str the-content)
+         [:span.uk-label.uk-label-danger.uk-position-right (cond
+                                                            (map? (get-in @editor-atom (vec (conj @show-me-da-way the-key))))
+                                                            "map"
+                                                            (vector? (get-in @editor-atom (vec (conj @show-me-da-way the-key))))
+                                                            "vector"
+                                                            :else "string")]]))))
 
 
 
@@ -99,12 +99,12 @@
 (defn walk-edn [the-map]
   [:div
    (if (vector? (get-in the-map @show-me-da-way))
-     (map-indexed #(-> ^{:key %1}[one-array-item %1 %2])
+     (map-indexed #(-> ^{:key %1} [one-array-item %1 %2])
                   (if (= [] @show-me-da-way)
                     the-map
                     (get-in the-map @show-me-da-way)))
 
-     (map-indexed #(-> ^{:key (first %2)}[one-key-and-content %2])
+     (map-indexed #(-> ^{:key (first %2)} [one-key-and-content %2])
                   (if (= [] @show-me-da-way)
                     the-map
                     (get-in the-map @show-me-da-way))))])
@@ -114,7 +114,7 @@
     (fn []
       [:div.uk-card.uk-card-secondary
        [:div.uk-card-body.uk-padding-small
-        [:form.uk-form.uk-flex-center.uk-margin-remove {:data-uk-grid  true}
+        [:form.uk-form.uk-flex-center.uk-margin-remove {:data-uk-grid true}
          [:textarea.uk-textarea.uk-width-1-1.uk-text-center.uk-margin-remove.uk-padding-remove {:rows 5 :placeholder "ADD EDN" :on-change #(reset! edn-atom (-> % .-target .-value))}]]
         [:button.uk-button.uk-button-secondary.uk-width-1-1.uk-margin-remove {:on-click #(do
                                                                                            (reset! show-me-da-way [])
@@ -153,7 +153,7 @@
      [:div.uk-width-4-5.uk-text-center.uk-text-large.uk-padding-small "Value"]]
     [walk-edn @editor-atom]
     [result]]])
-   ;[:div [:input.uk-input.uk-input-default {:placeholder "EDN" :on-change #(reset! edn-atom (-> % .-target .-value))}]]])
+;[:div [:input.uk-input.uk-input-default {:placeholder "EDN" :on-change #(reset! edn-atom (-> % .-target .-value))}]]])
 
 
 ;; -------------------------
@@ -167,7 +167,7 @@
   [:div [@page]])
 
 (secretary/defroute "/" []
-  (reset! page #'home-page))
+                    (reset! page #'home-page))
 
 
 
